e08d108da79cd81d4e63ae82d737a612710fc3da
Marco Netto <marco.netto@gmail.com>
2024-03-29 14:33:21 -0700

    add gromacs script

---
+++ appsetup_gromacs.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+set -x
+
+MPI_EXE_PATH="${AZ_BATCH_NODE_MOUNTS_DIR}/data/"
+MPI_EXE=gmx_mpi
+#MPI_CODE=mpi_matrix_mult.c
+echo "MPI_EXE_PATH=$MPI_EXE_PATH"
+
+function setup_data {
+  if [[ -d GROMACS_TestCaseA ]]; then
+    echo "Data already exists"
+    return
+  fi
+  wget https://repository.prace-ri.eu/ueabs/GROMACS/2.2/GROMACS_TestCaseA.tar.xz
+  tar -xf GROMACS_TestCaseA.tar.xz
+}
+
+function generate_run_script {
+
+  cat <<EOF >run_app.sh
+#!/bin/bash
+
+MPI_EXE_PATH="\${AZ_BATCH_NODE_MOUNTS_DIR}/data/"
+
+set -x
+IFS=';' read -ra ADDR <<< "\$AZ_BATCH_NODE_LIST"
+
+source /cvmfs/software.eessi.io/versions/2023.06/init/bash
+module load GROMACS
+module load OpenMPI/4.1.6-GCC-13.2.0
+
+which gmx_mpi
+which mpirun
+
+echo "MPI_EXE_PATH=\$MPI_EXE_PATH"
+pwd
+cd \$MPI_EXE_PATH
+pwd
+execdir="run_\$((RANDOM % 90000 + 10000))"
+mkdir -p \$execdir
+cd \$execdir || exit
+echo "Execution directory: \$execdir"
+
+ln -s "\$HOME/GROMACS_TestCaseA/ion_channel.tpr" .
+
+# Create host file
+batch_hosts=hosts.batch
+rm -rf \$batch_hosts
+
+IFS=';' read -ra ADDR <<< "\$AZ_BATCH_NODE_LIST"
+
+[[ -z \$PPN ]] && echo "PPN not defined"
+PPN=\$PPN
+
+hostprocmap=""
+
+for host in "\${ADDR[@]}"; do
+    echo \$host >> \$batch_hosts
+    hostprocmap="\$hostprocmap,\$host:\${PPN}"
+done
+
+hostprocmap="\${hostprocmap:1}"
+
+NODES=\$(cat \$batch_hosts | wc -l)
+
+NP=\$((\$NODES*\$PPN))
+
+echo "NODES=\$NODES PPN=\$PPN"
+echo "hostprocmap=\$hostprocmap"
+set -x
+
+APP_EXE=\$(which gmx_mpi)
+echo "Running GROMACS with \$NP processes ..."
+export UCX_NET_DEVICES=mlx5_ib0:1
+#time mpirun -np \$NP --host \$hostprocmap \$APP_EXE
+
+#echo "WRF run completed... confirming"
+
+#cat rsl.error.0000
+#if [[ \$(grep "SUCCESS COMPLETE WRF" rsl.error.0000) ]]; then
+#  echo "WRF run completed successfully"
+#  exit 0
+#else
+#  echo "WRF run failed"
+#  exit 1
+#fi
+
+EOF
+  chmod +x run_app.sh
+}
+
+cd "$MPI_EXE_PATH" || exit
+setup_data
+generate_run_script
