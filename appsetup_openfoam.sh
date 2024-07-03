44219738266e14480bd9d9b5dcb63011f1afb13e
Marco Netto <marco.netto@gmail.com>
2024-07-03 09:38:08 -0700

    modify test

--- appsetup_openfoam.sh
+++ appsetup_openfoam.sh
@@ -42,8 +42,12 @@ echo "Execution directory: \$execdir"
 
 pwd
 
-[[ -z \$PPN ]] && echo "PPN not defined"
-PPN=\$PPN
+#[[ -z \$PPN ]] && echo "PPN not defined"
+#PPN=\$PPN
+echo "PPN=\$PPN"
+
+
+
 
 # Create host file
 batch_hosts=hosts.batch
@@ -51,9 +55,13 @@ rm -rf \$batch_hosts
 
 hostprocmap=""
 for host in "\${ADDR[@]}"; do
-    echo \$host >> \$batch_hosts
+    echo "\$host slots=\${PPN}" >> \$batch_hosts
     hostprocmap="\$hostprocmap,\$host:\${PPN}"
 done
+
+echo "hostfile start"
+cat \$batch_hosts
+echo "hostfile end"
 
 #hostprocmap="\${hostprocmap:1}"
 
@@ -64,8 +72,7 @@ NP=\$((\$NODES*\$PPN))
 echo "NODES=\$NODES PPN=\$PPN"
 echo "hostprocmap=\$hostprocmap"
 
-APP_EXE=\$(which gmx_mpi)
-echo "Running GROMACS with \$NP processes ..."
+echo "Running OpenFOAM with \$NP processes ..."
 export UCX_NET_DEVICES=mlx5_ib0:1
 
 #export OMP_NUM_THREADS=1
@@ -76,9 +83,9 @@ export OMPI_MCA_pml=ucx
 ########################### APP EXECUTION #####################################
 BLOCKMESH_DIMENSIONS="20 8 8" # 0.35M cells
 
-mpiopts="\$mpiopts -np \$NP --hostfile \$batch_hosts"
+mpiopts="\$mpiopts -np \$NP --hostfile \$batch_hosts --report-bindings"
 
-TASKS_PER_NODE=\$SLURM_NTASKS_PER_NODE
+#TASKS_PER_NODE=\$SLURM_NTASKS_PER_NODE
 NTASKS=\$NP
 
 X=\$((\$NTASKS / 4))
@@ -92,6 +99,7 @@ foamDictionary -entry "hierarchicalCoeffs/n" -set "( \$X \$Y \$Z )" system/decom
 foamDictionary -entry blocks -set "( hex ( 0 1 2 3 4 5 6 7 ) ( \$BLOCKMESH_DIMENSIONS ) simpleGrading ( 1 1 1 ) )" system/blockMeshDict
 
 time ./Allrun
+reconstructPar -constant
 #############################################################################
 
 
@@ -99,6 +107,7 @@ time ./Allrun
 LOGFILE="log.foamRun"
 if [[ -f \$LOGFILE && \$(tail -n 1 "\$LOGFILE") == 'Finalising parallel run' ]]; then
   echo "Simulation completed"
+  touch case.foam
   exit 0
 else
   echo "Simulation failed"
