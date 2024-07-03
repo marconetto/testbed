da97984405fc619e21f2311c2eb98c4f1d17658a
Marco Netto <marco.netto@gmail.com>
2024-07-03 12:15:13 -0700

    modify test

--- appsetup_openfoam.sh
+++ appsetup_openfoam.sh
@@ -50,7 +50,7 @@ echo "PPN=\$PPN"
 
 
 # Create host file
-batch_hosts=hosts.batch
+batch_hosts=hostfile
 rm -rf \$batch_hosts
 
 hostprocmap=""
@@ -88,9 +88,9 @@ sed -i '/decomposePar/a declare -f runParallel' Allrun
 #export FOAM_MPIRUN_FLAGS="--hostfile \$batch_hosts $(env | grep 'WM_\|FOAM' | cut -d'=' -f1 | sed 's/^/-x /g' | tr '\n' ' ') -x MPI_BUFFER_SIZE --report-bindings --map-by ppr:${ranks_per_numa}:numa"
 
 
-export FOAM_MPIRUN_FLAGS="-mca pml ucx $(env | grep 'WM_\|FOAM_' | cut -d'=' -f1 | sed 's/^/-x /g' | tr '\n' ' ') -x MPI_BUFFER_SIZE -x UCX_IB_MLX5_DEVX=n -x UCX_POSIX_USE_PROC_LINK=n -x PATH -x LD_LIBRARY_PATH --oversubscribe"
+#export FOAM_MPIRUN_FLAGS="-mca pml ucx $(env | grep 'WM_\|FOAM_' | cut -d'=' -f1 | sed 's/^/-x /g' | tr '\n' ' ') -x MPI_BUFFER_SIZE -x UCX_IB_MLX5_DEVX=n -x UCX_POSIX_USE_PROC_LINK=n -x PATH -x LD_LIBRARY_PATH --oversubscribe"
 
-#export FOAM_MPIRUN_FLAGS="--hostfile \$batch_hosts --verbose"
+export FOAM_MPIRUN_FLAGS="--hostfile hostfile --verbose"
 #export FOAM_MPIRUN_FLAGS="--hostfile \$batch_hosts --report-bindings --verbose"
 echo \$FOAM_MPIRUN_FLAGS
 
