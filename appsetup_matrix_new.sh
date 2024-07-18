7884f94982fe597f9bde5ba87605ef32fa330fb2
Marco Netto <marco.netto@gmail.com>
2024-07-18 08:26:33 -0700

    add test

---
+++ appsetup_matrix_new.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+export MPI_EXE=mpi_matrix_mult
+export MPI_CODE=mpi_matrix_mult.c
+export MPI_EXE_PATH="${AZ_BATCH_NODE_MOUNTS_DIR}/data/"
+
+[[ -f /etc/bashrc ]] && . /etc/bashrc
+
+source /etc/profile.d/modules.sh
+module load gcc-9.2.0
+module load mpi/hpcx
+
+main_setup() {
+  echo "main setup"
+
+  set -x
+
+  CODEURL=https://raw.githubusercontent.com/marconetto/testbed/main/mpi_matrix_mult.c
+  curl -sL $CODEURL -o "$MPI_EXE_PATH"/$MPI_CODE
+
+  which mpicc
+
+  mpicc -o ${MPI_EXE} ${MPI_CODE}
+  ls -l ${MPI_EXE}
+}
+
+main_run() {
+  echo "main run"
+
+  [[ -z $APPINTERACTIONS ]] && APPINTERACTIONS=5
+  [[ -z $APPMATRIXSIZE ]] && APPMATRIXSIZE=1000
+
+  NP=$(($NODES * $PPN))
+  set
+  set -x
+  mpirun -np $NP --host "$AZ_HOST_LIST_PPN" --map-by ppr:"${PPN}":node /mnt/resource/batch/tasks/fsmounts/data//mpi_matrix_mult "${APPMATRIXSIZE}" "${APPINTERACTIONS}"
+}
